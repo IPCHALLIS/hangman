b88682dc0e5227fb3b2ec63b3d74dcb3ff9d8f4d
Isaac Challis <isaac.challis@gmail.com>
2023-01-25 14:52:26 +0000

    Checks if the user input is a single character and is alphabtical.

--- milestone_2.py
+++ milestone_2.py
@@ -8,3 +8,9 @@ word = random.choice(word_list)
 print(word)
 # %%
 guess = input("Please enter a single letter: ")
+# %%
+if len(guess) == 1 and guess.isalpha():
+    print("Good Guess!")
+else:
+    print("Oops! That is not a valid input.")
+# %%
