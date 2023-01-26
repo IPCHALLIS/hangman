97c26ccd1d7eb2d0c1621a021d62a229e7169ea9
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 21:17:17 +0000

    Modifying the instance.

--- milestone_4.py
+++ milestone_4.py
@@ -8,3 +8,21 @@ class Hangman:
         self.num_lives = num_lives
         self.word_list = ["orange", "mango", "peach", "pineapple", "kiwi"]
         self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+
+    def ask_for_input(self):
+        check_guess(guess)
+        while True:
+            guess = input("Please enter a single, alphabetical character: ")
+            if len(guess) == 1 and guess.isalpha():
+                break
+            else:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+
+hng1 = Hangman(["orange", "mango", "peach", "pineapple", "kiwi"])
