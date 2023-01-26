b73478a03a756e1efe609f37ec1301c432906f24
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 10:31:30 +0000

    Creates 2 functions, check_guess and ask_for_input wich contain the code blocks above and runs checks for the user's input.

--- milestone_3.py
+++ milestone_3.py
@@ -9,3 +9,21 @@ if guess in word:
     print(f"Good guess! {guess} is in the word.")
 else:
     print(f"Sorry, {guess} is not in the word. Try again.")
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    check_guess(guess)
+    while True:
+        guess = input("Please enter a single, alphabetical character: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+ask_for_input()
