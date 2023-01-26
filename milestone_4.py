ab6344e43855bed092de0c3c91589458a515a90e
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 21:26:32 +0000

    prefix

--- milestone_4.py
+++ milestone_4.py
@@ -2,7 +2,7 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word = random.choice(word_list)
+        self.word = random.choice(self.word_list)
         self.word_guessed = ['_']*len(self.word)
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
