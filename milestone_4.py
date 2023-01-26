c1c0e6571734b2aea9ff88d13d13cd34a5a7b314
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 13:56:16 +0000

    Modifies the attributes of the Hangman Class.

--- milestone_4.py
+++ milestone_4.py
@@ -3,8 +3,8 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        self.word_guessed = []
+        self.word_guessed = "_"*len(word)
         self.num_letters = len(word)
         self.num_lives = num_lives
         self.word_list = ["orange", "mango", "peach", "pineapple", "kiwi"]
-        self.list_of_guesses = list_of_guesses
+        self.list_of_guesses = []
