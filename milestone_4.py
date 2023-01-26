87345f2d8bfd771e78774c5027409cb227908ed5
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 16:30:34 +0000

    Redefined num_letters with 'len(set(word))' to find the number of unique characters.

--- milestone_4.py
+++ milestone_4.py
@@ -2,9 +2,9 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word = random.choice(word_list)
+        word = random.choice(word_list)
         self.word_guessed = ['_']*len(word)
-        self.num_letters = len(word)
+        self.num_letters = len(set(word))
         self.num_lives = num_lives
         self.word_list = ["orange", "mango", "peach", "pineapple", "kiwi"]
         self.list_of_guesses = []
