0ac8e63b59f645096c9855d22a052fadaeb7951e
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 15:30:45 +0000

    Redefines the 'word' attribute in the class preceded by the '.self' prefix.

--- milestone_4.py
+++ milestone_4.py
@@ -2,7 +2,7 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        word = random.choice(word_list)
+        self.word = random.choice(word_list)
         self.word_guessed = "_"*len(word)
         self.num_letters = len(word)
         self.num_lives = num_lives
