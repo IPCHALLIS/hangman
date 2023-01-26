7f3757f658d9a32dd656288d363e5fad7519faa5
Isaac Challis <isaac.challis@gmail.com>
2023-01-26 17:56:59 +0000

    Adds 2 print statements to lines 11 and 12.

--- milestone_4.py
+++ milestone_4.py
@@ -8,20 +8,6 @@ class Hangman:
         self.num_lives = num_lives
         self.word_list = ["orange", "mango", "peach", "pineapple", "kiwi"]
         self.list_of_guesses = []
-
-    def check_guess(guess):
-        guess = guess.lower()
-        if guess in word:
-            print(f"Good guess! {guess} is in the word.")
-        else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
-
-    def ask_for_input():
-        check_guess(guess)
-        while True:
-            guess = input("Please enter a single, alphabetical character: ")
-            if len(guess) == 1 and guess.isalpha():
-                break
-            else:
-                print("Invalid letter. Please, enter a single alphabetical character.")
-
+        print(f"The mistery word has {num_letters} characters")
+        print(f"{word_guessed}")
+        pass
