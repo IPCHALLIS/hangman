0f50d2144aec3dc7cf010eb1ce4c4c4e5b9bf4f4
Isaac Challis <isaac.challis@gmail.com>
2023-01-27 11:43:30 +0000

    Adding break to line 26 to avoid infinite 'loop'.

--- milestone_4.py
+++ milestone_4.py
@@ -23,7 +23,7 @@ class Hangman:
                 print("You already tried that letter!")
             else:
                 self.check_guess(guess)
-
+                break
 
 
 word_list = ["orange", "mango", "peach", "pineapple", "kiwi"]
