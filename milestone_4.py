ca3af5658ea92445c1e67e5a43c765664c79fb64
Isaac Challis <isaac.challis@gmail.com>
2023-01-27 11:25:14 +0000

    Redefining 'word' in line 14 because of NameError.

--- milestone_4.py
+++ milestone_4.py
@@ -11,7 +11,7 @@ class Hangman:
 
     def check_guess(self, guess):
         guess = guess.lower()
-        if guess in word:
+        if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
